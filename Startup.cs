2486e95459b2e30efe1437ab1da4b67ba0f1b34c
Cary Ford <carymichaelford@gmail.com>
2014-11-13 19:04:09 -0800

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhiteWaveApp.Startup))]
+namespace WhiteWaveApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
